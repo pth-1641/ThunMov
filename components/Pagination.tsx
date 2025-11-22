183cdad5128b50a1f25b2923675a169c51b3b33a
pth-1641 <asasasa11221176@gmail.com>
2025-11-22 13:07:36 +0700

    fix: udpate pagination on change route

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -11,33 +11,37 @@ type PaginationProps = {
   totalItemsPerPage: number;
 };
 
-export const Pagination: FC<PaginationProps> = (props) => {
-  const { currentPage = 1, totalItems, totalItemsPerPage } = props;
+export const Pagination: FC<PaginationProps> = ({
+  currentPage = 1,
+  totalItems,
+  totalItemsPerPage,
+}) => {
+  const [jumpPage, setJumpPage] = useState<number>(1);
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const { push } = useRouter();
-
-  const [currentRoute, setCurrentRoute] = useState<string>("");
-  const [jumpPage, setJumpPage] = useState<number>(currentPage);
 
   const totalPages = useMemo(() => {
     if (!totalItems || !totalItemsPerPage) return 0;
     return Math.ceil(totalItems / totalItemsPerPage);
   }, [totalItems, totalItemsPerPage]);
 
-  useEffect(() => {
+  const currentRoute = useMemo(() => {
     const q = searchParams.get("q");
-    const url = q ? `${pathname}?q=${q}&` : `${pathname}?`;
-    setCurrentRoute(url);
-  }, [pathname]);
+    return q ? `${pathname}?q=${q}&` : `${pathname}?`;
+  }, [pathname, searchParams]);
+
+  useEffect(() => {
+    setJumpPage(currentPage);
+  }, [currentPage]);
 
   if (currentPage > totalPages) notFound();
   return (
     <ul className="flex mt-20 font-medium justify-center space-x-2">
       <Link
         href={`${currentRoute}page=${currentPage - 1}`}
-        className={`size-11 flex items-center rounded-full bg-white/10 justify-center text-white duration-300 hover:text-black hover:bg-primary${
-          currentPage > 1 ? "opacity-100" : "opacity-75 pointer-events-none"
+        className={`size-11 flex items-center justify-center rounded-full bg-white/10 text-white duration-300 hover:text-black hover:bg-primary ${
+          currentPage > 1 ? "" : "opacity-50 pointer-events-none"
         }`}
       >
         <Icon icon="icon-park-outline:left" height={24} />
@@ -47,16 +51,17 @@ export const Pagination: FC<PaginationProps> = (props) => {
         className="flex items-center gap-2 rounded-full bg-white/15 h-11 px-5"
         onSubmit={(e) => {
           e.preventDefault();
-          push(`${currentRoute}page=${jumpPage}`);
+          if (jumpPage >= 1 && jumpPage <= totalPages) {
+            push(`${currentRoute}page=${jumpPage}`);
+          }
         }}
       >
         <input
           className="w-full max-w-12 text-center outline-none bg-transparent border border-primary/50 py-0.5 rounded px-1"
           value={jumpPage}
-          type="text"
           onChange={(e) => {
-            const value = +e.target.value;
-            if (!isNaN(value) && value <= totalPages) setJumpPage(value);
+            const value = Number(e.target.value);
+            if (!isNaN(value)) setJumpPage(value);
           }}
         />
         <p> / {totalPages}</p>
@@ -64,10 +69,8 @@ export const Pagination: FC<PaginationProps> = (props) => {
 
       <Link
         href={`${currentRoute}page=${currentPage + 1}`}
-        className={`size-11 flex items-center rounded-full bg-white/10 justify-center text-white duration-300 hover:text-black hover:bg-primary ${
-          currentPage < totalPages
-            ? "opacity-100"
-            : "opacity-75 pointer-events-none"
+        className={`size-11 flex items-center justify-center rounded-full bg-white/10 text-white duration-300 hover:text-black hover:bg-primary ${
+          currentPage < totalPages ? "" : "opacity-50 pointer-events-none"
         }`}
       >
         <Icon icon="icon-park-outline:right" height={24} />
